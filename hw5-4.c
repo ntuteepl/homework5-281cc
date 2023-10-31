58071f57ceff40b3d55467d557c0da297b1419c2
281cc <146065635+281cc@users.noreply.github.com>
2023-10-31 14:43:12 +0800

    Update hw5-4.c

--- hw5-4.c
+++ hw5-4.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+main()
+{
+    int a,b;
+    float c,d,e;
+    scanf("%d%d",&a,&b);
+    c=a*30+b*0.5;
+    d=b*6;
+    e=c-d;
+    if(e<0){
+        e=e+180;
+    }
+    if(e>=180){
+        e=e-180;
+    }
+    printf("%.3f\n",e);
+}
